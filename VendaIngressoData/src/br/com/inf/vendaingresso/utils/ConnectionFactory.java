dba21f3892fc544f581f224e790df1d89f5c8a8d
danilolm <danilolopesdemoraes@gmail.com>
2014-05-14 23:08:44 -0300

    Connection oracle

--- VendaIngressoData/src/br/com/inf/vendaingresso/utils/ConnectionFactory.java
+++ VendaIngressoData/src/br/com/inf/vendaingresso/utils/ConnectionFactory.java
@@ -10,10 +10,10 @@ import java.sql.SQLException;
  */
 public class ConnectionFactory {
     
-    private static final String DRIVER = "";
-    private static final String URL = "";
-    private static final String PASSWORD = "";
-    private static final String USER = "";
+    private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String URL = "jdbc:oracle:thin:@//inforplace.no-ip.org:1521/pos";
+    private static final String PASSWORD = "pos#123";
+    private static final String USER = "pos";
     
     public static Connection getConnection() 
             throws ClassNotFoundException, SQLException{

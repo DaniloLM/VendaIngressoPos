97cdcaffc71484dbb8662be416d78df57eed0656
marcuswoc <marcuswoc@gmail.com>
2014-05-16 22:23:06 -0300

    new file:   VendaIngressoExec/build.xml new file:   VendaIngressoExec/build/classes/.netbeans_automatic_build new file:   VendaIngressoExec/build/classes/.netbeans_update_resources new file:   VendaIngressoExec/build/classes/vendaingressoexec/VendaIngressoExec.class new file:   VendaIngressoExec/manifest.mf new file:   VendaIngressoExec/nbproject/build-impl.xml new file:   VendaIngressoExec/nbproject/genfiles.properties new file:   VendaIngressoExec/nbproject/private/private.properties new file:   VendaIngressoExec/nbproject/private/profiler/configurations.xml new file:   VendaIngressoExec/nbproject/project.properties new file:   VendaIngressoExec/nbproject/project.xml new file:   VendaIngressoExec/src/vendaingressoexec/VendaIngressoExec.java

---
+++ VendaIngressoExec/src/vendaingressoexec/VendaIngressoExec.java
@@ -0,0 +1,120 @@
+package vendaingressoexec;
+
+import java.util.Scanner;
+
+public class VendaIngressoExec {
+
+    static int conta_vendas;
+    static double caixa;
+
+    public static void main(String args[]) {
+        System.out.println("###################################################################");
+        System.out.println("######################### SEJA BEM VINDO ##########################");
+        System.out.println("############## POR FAVOR INFORME OS DADOS SOLICITADOS #############");
+        System.out.println("###################################################################");
+        menuprincipal();
+    }
+
+    //MENU PRINCIPAL
+    public static void menuprincipal() {
+        Scanner input = new Scanner(System.in);
+        int sair = 0;
+        while (sair != 4) {
+            System.out.println("");
+            System.out.println("##############################################");
+            System.out.println("SELECIONE UMA DAS OPÇÕES DO MENU");
+            System.out.println("(1) - VENDAS");
+            System.out.println("(2) - CADASTRAR EVENTOS");
+            System.out.println("(3) - CADASTRAR FUNCIONARIO");
+            System.out.println("(4) - SAIR");
+            System.out.println("##############################################");
+            int menuprincipal = input.nextInt();
+            switch (menuprincipal) {
+                case 1: {
+                    menuvendas();
+                    break;
+                }
+                case 2: {
+                    cadastrareventos();
+                    break;
+                }
+                case 3: {
+                    cadastrarfuncionario();
+                    break;
+                }
+                case 4: {
+                    sairsistema();
+                }
+                default:
+                    System.out.println("Esta não é uma opção válida!");
+            }
+        }
+    }
+
+    //OPÇÕES DO MENU "PRINCIPAL"
+    public static void menuvendas() {
+        int sair = 0;
+        while (sair != 3 || sair != 4) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("");
+            System.out.println("#VENDAS#");
+            System.out.println("##############################################");
+            System.out.println("(1) - VENDER INGRESSO");
+            System.out.println("(2) - CANCELAR VENDA");
+            System.out.println("(3) - RETORNAR AO MENU PRINCIPAL");
+            System.out.println("(4) - SAIR");
+            System.out.println("##############################################");
+            int menusecundario = input.nextInt();
+            switch (menusecundario) {
+                case 1: {
+                    venderingresso();
+                    break;
+                }
+                case 2: {
+                    cancelarvenda();
+                    break;
+                }
+                case 3: {
+                    retornarmenuprincipal();
+                    sair = 3;
+                    break;
+                }
+                case 4: {
+                    sairsistema();
+                    sair = 4;
+                }
+                default:
+                    System.out.println("Esta não é uma opção válida!");
+            }
+        }
+    }
+
+    public static void cadastrareventos() {
+
+    }
+
+    public static void cadastrarfuncionario() {
+
+    }
+
+    public static void sairsistema() {
+        System.exit(0);
+    }
+
+    //OPÇÕES DO MENU "VENDAS"
+    public static void venderingresso() {
+
+    }
+
+    public static void cancelarvenda() {
+
+    }
+
+    public static void retornarmenuprincipal() {
+        System.out.println("");
+        System.out.println("");
+        System.out.print("#MENU PRINCIPAL#");
+        menuprincipal();
+    }
+
+}

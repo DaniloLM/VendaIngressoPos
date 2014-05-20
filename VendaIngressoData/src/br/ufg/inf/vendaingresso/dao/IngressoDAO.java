e61b0a88bba5da70883c097cba39307c29363bc4
danilolm <danilolopesdemoraes@gmail.com>
2014-05-20 20:56:31 -0300

    merge correcao

--- VendaIngressoData/src/br/ufg/inf/vendaingresso/dao/IngressoDAO.java
+++ VendaIngressoData/src/br/ufg/inf/vendaingresso/dao/IngressoDAO.java
@@ -10,12 +10,8 @@ import java.util.List;
  */
 public interface IngressoDAO {
     public void salvar(Ingresso ingresso, Secao secao, Evento evento);
-<<<<<<< HEAD
-    public int getVendidosTotal();
-=======
     public void atualizar(Cliente cliente);
-    public ResultSet getVendidosTotal();
->>>>>>> 8e8b02d1764a220a475d8243bd2ce657a9a8c261
+    public int getVendidosTotal();
     public ResultSet getVendidosSecao(Secao secao, Evento evento);
     public ResultSet getVendidosEvento(Evento evento);
     public List<Ingresso> getIngressoDisponiveis(Evento evento);
